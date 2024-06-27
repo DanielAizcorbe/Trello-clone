d72466c9a49b85ea4c2ad6089efd1831545e304f
DanielAizcorbe <daniel.aizcorbe1@gmail.com>
2024-06-27 17:05:18 -0300

    add ruta "api/tableros/:id" con get y delete

--- src/app/api/tableros/[...id]/route.ts
+++ src/app/api/tableros/[...id]/route.ts
@@ -1,7 +1,16 @@
-import {NextRequest, NextResponse} from "next/server";
+import { TableroRepo } from "@/db-repositories/TableroRepo";
+import { NextRequest, NextResponse } from "next/server";
+
+const repo = new TableroRepo();
 
 export async function GET(request: NextRequest, params: any) {
-    const id = params.params.id
-    console.log(id)
-    return NextResponse.json(id);
+    const id = params.params.id[0]
+    const tablero = await repo.getTablero(id);
+    return NextResponse.json(tablero);
+}
+
+export async function DELETE(request: NextRequest, params: any) {
+    const id = params.params.id[0]
+    const tablero = await repo.deteleTablero(id);
+    return NextResponse.json(tablero);
 }
